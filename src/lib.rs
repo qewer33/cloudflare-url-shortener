9ebe9f20e1244060386f6c2995fa369d4cda54c5
qewer33 <qewer@qewer.dev>
2022-12-05 20:12:35 +0300

    Add user interface

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,11 @@ struct DeleteRequest {
 struct UrlShortener;
 
 impl UrlShortener {
+    fn serve_index(_: Request, _: RouteContext<()>) -> Result<Response> {
+        let html = include_str!("index.html");
+        Response::from_html(html)
+    }
+
     async fn redirect(mut req: Request, ctx: RouteContext<()>) -> Result<Response> {
         if let Some(key) = ctx.param("key") {
             let url = ctx.kv("KV")?.get(key.trim()).text().await;
@@ -42,41 +47,7 @@ impl UrlShortener {
                 .replace("${url}", url.as_str());
                 return Response::from_html(html);
             }
-            let html404 = r#"
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="utf-8">
-                <title>404</title>
-            </head>
-            <body>
-                <h1>Seems like this short URL doesn't exit anymore...</h1>
-                <a href="https://qewer.dev">CLICK HERE TO GO TO MY HOMEPAGE</a>
-            </body>
-            </html>
-            <style>
-                body {
-                    height: 100vh;
-                    display: flex;
-                    flex-direction: column;
-                    font-family: Arial, sans-serif;
-                    background-color: #1D1D1D;
-                    justify-content: center;
-                    align-items: center;
-                }
-                h1 {
-                    color: #FFFFFF;
-                }
-                a {
-                    color: #FFFFFF;
-                    background-color: #0578F0;
-                    transform: skew(-12deg);
-                    padding: 15px 30px;
-                    text-decoration: none;
-                    font-weight: 900;
-                }
-            </style>
-            "#;
+            let html404 = include_str!("404.html");
             return Response::from_html(html404);
         }
         Response::error("Bad Request: Missing Parameter", 400)
@@ -139,9 +110,7 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
     let router = Router::new();
 
     router
-        .get("/", |_, _| {
-            Response::from_html("Hello from Qewer's URL shortener!")
-        })
+        .get("/", UrlShortener::serve_index)
         .get_async("/:key", UrlShortener::redirect)
         .post_async("/create/:secret", UrlShortener::create)
         .post_async("/delete/:secret", UrlShortener::delete)
